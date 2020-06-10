6a1733e85ed686b222162724a95726f4c8784cb7
Damon Wu <damonwu0605@gmail.com>
2020-06-10 19:39:26 +1000

    change db settings on heroku

--- crm1/settings.py
+++ crm1/settings.py
@@ -77,23 +77,29 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_demo',
-        'USER': '*********',
-        'PASSWORD': '*********',
-        'HOST': '*********',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+
+# Postgres SQL DB implementation (RDS version)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'django_demo',
+#         'USER': '*********',
+#         'PASSWORD': '*********',
+#         'HOST': '*********',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
